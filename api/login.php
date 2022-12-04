ff594b66309a648eaf4700c5b411979f5559cd57
Kai Uerlichs <2421101@dundee.ac.uk>
2022-12-04 21:48:49 +0000

    Login/out to json

--- api/login.php
+++ api/login.php
@@ -1,59 +1,68 @@
 <?php
-    require_once( __DIR__ . "./Database.php");
+require_once(__DIR__ . "./Database.php");
 
-    $db = new Database();
-    $db->connect();
+$db = new Database();
+$db->connect();
 
-    session_start();
-    header('Content-Type: application/json; charset=utf-8');
+$_POST = json_decode(file_get_contents('php://input'), true);
 
-    if(!isset($_SESSION["loggedIn"])){
-        // Log user in
-        if(isset($_POST["email"]) and isset($_POST["password"])){
-            $email = $_POST["email"];
-            $password = hash("sha256", $_POST["password"]);
+session_start();
+header('Content-Type: application/json; charset=utf-8');
 
-            try{
-                if($db->GetPasswordHash($email) == $password){
-                    // Correct password
-                    $_SESSION["email"] = $email;
-                    $_SESSION["loggedIn"] = true;
+if (!isset($_SESSION["loggedIn"])) {
+    // Log user in
+    if (isset($_POST["email"]) and isset($_POST["password"])) {
+        $email = $_POST["email"];
+        $password = hash("sha256", $_POST["password"]);
 
-                    echo json_encode(array(
+        try {
+            if ($db->GetPasswordHash($email) == $password) {
+                // Correct password
+                $_SESSION["email"] = $email;
+                $_SESSION["loggedIn"] = true;
+
+                echo json_encode(
+                    array(
                         "message" => "Login successful",
                         "user" => $email,
                         "code" => 101
-                    ));
-                }
-                else{
-                    // Incorrect password
-                    echo json_encode(array(
+                    )
+                );
+            } else {
+                // Incorrect password
+                echo json_encode(
+                    array(
                         "error" => "Password incorrect",
                         "code" => 104
-                    ));
-                }
+                    )
+                );
             }
-            catch(InvalidArgumentException $e) {
-                echo json_encode(array(
+        } catch (InvalidArgumentException $e) {
+            echo json_encode(
+                array(
                     "error" => "No user with specified email",
-                    "code"=> 103
-                ));
-                session_destroy();
-            }
+                    "code" => 103
+                )
+            );
+            session_destroy();
         }
+    }
 
-        // Missing parameters
-        else{
-            echo json_encode(array(
+    // Missing parameters
+    else {
+        echo json_encode(
+            array(
                 "error" => "Missing email or password",
                 "code" => 102
-            ));
-        }
+            )
+        );
     }
-    else{
-        // User already logged in
-        echo json_encode(array(
+} else {
+    // User already logged in
+    echo json_encode(
+        array(
             "error" => "Already logged in",
             "code" => 100
-        ));
-    }
+        )
+    );
+}
