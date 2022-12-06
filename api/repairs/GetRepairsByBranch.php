c3662cf39980b8ff2a8278a911190109bca14e19
jlyons4593 <jlyons4593@gmail.com>
2022-12-06 23:10:48 +0000

    added get repairs by branchid

---
+++ api/repairs/GetRepairsByBranch.php
@@ -0,0 +1,45 @@
+<?php
+require_once(__DIR__ . "/../Database.php");
+
+$db = new Database();
+$db->connect();
+
+
+
+header('Content-Type: application/json; charset=utf-8');
+
+if(!isset($_SESSION["loggedIn"])){
+    // User not logged in
+    echo json_encode(
+        array(
+            "error" => "User not logged in.",
+            "code" => 200
+        )
+    );
+    return;
+}
+if ($_SESSION['Permissions']!=2) {
+    // User not logged in
+    echo json_encode(
+        array(
+            "error" => "User has no permissions.",
+            "code" => 209
+        )
+    );
+    return;
+}
+
+try{
+    $repairs = $db->getProductByID($_GET["branchid"]);
+
+    // Order created successfully
+    echo json_encode($repairs);
+}
+catch(PDOException $e){
+    // Processing errors
+    echo json_encode(array(
+        "error" => "Internal error processing order.",
+        "code" => 305
+    ));
+    return;
+}
