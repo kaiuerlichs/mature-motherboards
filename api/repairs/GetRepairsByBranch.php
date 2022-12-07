bbc19bf4db91202318ca37e8f4069c562e989e9d
jlyons4593 <jlyons4593@gmail.com>
2022-12-07 14:15:20 +0000

    fixed verification

--- api/repairs/GetRepairsByBranch.php
+++ api/repairs/GetRepairsByBranch.php
@@ -18,7 +18,7 @@ if(!isset($_SESSION["loggedIn"])){
     );
     return;
 }
-if ($_SESSION['Permissions']==2) {
+if ($_SESSION['Permissions']==3) {
     // User not logged in
     echo json_encode(
         array(
