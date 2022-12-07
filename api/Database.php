ee2bd371f4547a46587b02492c0743478d7ad340
jlyons4593 <jlyons4593@gmail.com>
2022-12-07 14:19:24 +0000

    added phone and email

--- api/Database.php
+++ api/Database.php
@@ -571,7 +571,7 @@ class Database
 
         // Get item information
         try {
-            $q = $this->connection->prepare("SELECT EmployeeID, FirstName, LastName FROM employee;");
+            $q = $this->connection->prepare("SELECT EmployeeID, FirstName, LastName, Email, Phone FROM employee;");
             if (!$q->execute()) {
                 throw new PDOException();
             }
