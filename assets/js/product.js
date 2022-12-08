e53bfe634c01f9d848682b1f8b90bf87929df7cf
Ross Coombs <2410466@dundee.ac.uk>
2022-12-08 13:12:49 +0000

    Add to cart confirmation + loading wheel

--- assets/js/product.js
+++ assets/js/product.js
@@ -5,6 +5,9 @@ try {
 }
 
 function onClick() {
+    document.getElementById("addToCart").innerHTML = '<i class="fa-solid fa-gear" id="spinner"></i>';
+    let spinner = document.getElementById("spinner");
+    spinner.classList.add("fa-spin");
     let alreadyInCart = false;
     let targetID = window.location.href.split("?")[1];
 
@@ -13,6 +16,11 @@ function onClick() {
             cart[id] = cart[id] + 1;
             alreadyInCart = true;
             console.log(cart[id]);
+            spinner.classList.remove("fa-spin")
+            document.getElementById("addToCart").innerHTML = 'Add To Cart';
+            document.getElementById("alert").classList.add("alert-danger");
+            document.getElementById("alert").innerHTML = "Already Added!";
+            document.getElementById("alert").removeAttribute("hidden");
             break;
         }
     }
@@ -21,6 +29,11 @@ function onClick() {
             cart = {};
         }
         cart = Object.assign(cart, { [targetID]: 1 });
+        spinner.classList.remove("fa-spin")
+        document.getElementById("addToCart").innerHTML = 'Add To Cart';
+        document.getElementById("alert").classList.add("alert-primary");
+        document.getElementById("alert").innerHTML = "Added Successfully!";
+        document.getElementById("alert").removeAttribute("hidden");
 
     }
     localStorage.setItem('cart', JSON.stringify(cart));
