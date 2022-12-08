859df80ef5674daf93470d7585a264f286292b0d
HeatherCurrie <heathercurrie1000@gmail.com>
2022-12-08 16:50:59 +0000

    master :O

--- assets/js/order.js
+++
@@ -1,7 +0,0 @@
-var order = ["Hawkhill", "15/09/2022"];
-
-// Put into storage
-localStorage.setItem('order', JSON.stringify(order));
-
-// Retrieve the object from storage
-var retrievedObject = JSON.parse(localStorage.getItem('order'));
